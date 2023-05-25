d0f1883483c388f7d2cb2f0c43d2b897518ca39a
Ahmed Sobhy <105889007+AhmedSobhyToR@users.noreply.github.com>
2023-05-25 12:18:00 +0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.home-image');
+let container = document.querySelector('.container');
+
+function phones(phone){
+    img.src = phone;
+}
+
+function background(color){
+    container.style.background = color;
+}
+
